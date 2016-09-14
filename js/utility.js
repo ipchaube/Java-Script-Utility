6ca6f3868716b2bd103fe12e3580f681b89388de
Indu Prakash <prakash.indu@gmail.com>
2016-09-14 16:08:48 -0700

    Get current time Javascript

--- js/utility.js
+++ js/utility.js
@@ -9,3 +9,16 @@ function multiLineAlert(args){
     }
 
 }
+
+
+/*
+ * Get current time Javascript
+ */
+function getCurrentTime() {
+    var d = new Date();
+    var x = document.getElementById("demo");
+    var h = addZero(d.getHours());
+    var m = addZero(d.getMinutes());
+    var s = addZero(d.getSeconds());
+    return h + ":" + m + ":" + s;
+}
